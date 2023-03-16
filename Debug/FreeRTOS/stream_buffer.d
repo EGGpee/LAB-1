e66a1173aa61ee77c44f7a1e834ac92c15aad26f
EGGpee <peter410171@gmail.com>
2023-03-16 15:18:04 +0800

    LAB 1

---
+++ Debug/FreeRTOS/stream_buffer.d
@@ -0,0 +1,21 @@
+FreeRTOS/stream_buffer.o: ../FreeRTOS/stream_buffer.c \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/stream_buffer.h
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/stream_buffer.h:
---
+++ Debug/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+FreeRTOS/tasks.o: ../FreeRTOS/tasks.c \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/timers.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/stack_macros.h
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/timers.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/task.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/stack_macros.h:
